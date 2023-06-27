c724c5f0d1e93a37a2a9c67faa3e45254f0df245
Orenoid <stefu1225@gmail.com>
2023-06-28 01:06:09 +0800

    feat: 调整首页布局

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,15 @@ import Player from '@/components/player'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function App() {
   return (
-    <>
-      <header>
-        <h1>AI Player</h1>
+    <div class="flex flex-col items-center bg-[#2c2c32] h-screen px-44">
+      <header class="flex flex-row self-start mt-16 mb-8">
+        <h1 class="text-white text-5xl">AI Player</h1>
       </header>
-      <main>
+      <main class="bg-[#1f1f20] w-full aspect-video">
         <Player></Player>
       </main>
-    </>
+    </div>
   )
 }
